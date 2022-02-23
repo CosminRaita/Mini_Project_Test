880b048ef046380f3f2f8f0f81a7685e02bf23d3
PieckaAndrej <93518895+PieckaAndrej@users.noreply.github.com>
2022-02-23 14:20:33 +0100

    addpayment tests

--- PayStation/src/test/TestCalculationCurrencyDkk.java
+++ PayStation/src/test/TestCalculationCurrencyDkk.java
@@ -1,9 +1,14 @@
 package test;
 
-import org.junit.*;
-import static org.junit.Assert.*;
 
-import controllayer.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controllayer.ControlPayStation;
+import controllayer.IllegalCoinException;
 import modellayer.Currency;
 
 /**
@@ -16,7 +21,7 @@ public class TestCalculationCurrencyDkk {
 	ControlPayStation ps;
 
 	/** Fixture for pay station testing. */
-	@Before
+	@BeforeEach
 	public void setUp() {
 		ps = new ControlPayStation();
 	}
@@ -37,13 +42,124 @@ public class TestCalculationCurrencyDkk {
 		ps.addPayment(coinValue, coinCurrency, coinType);
 			
 		// Assert
-		assertEquals("Should display 3 min for 50 øre", expectedParkingTime, ps.readDisplay());
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 1 dkk should make the display report 6 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay6MinFor1dkk() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 6;	// In minutes
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.DKK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 1 euro should make the display report 40 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay40MinFor1Euro() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 40;	// In minutes
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 50 cents should make the display report 20 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay20MinFor50cents() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 20;	// In minutes
+		int coinValue = 50;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 1 euro and 1 dkk should make the display report 46 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay46MinFor1DkkAnd1Euro() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 46;	// In minutes
+		
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrencyEuro = Currency.ValidCurrency.EURO;
+		Currency.ValidCurrency coinCurrencyDkk = Currency.ValidCurrency.DKK;
+		
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 1 euro, 1 dkk, 50 cents and 50 ore should make the display report 68 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay68MinFor1DkkAnd1EuroAnd50centsAnd50ore() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 68;	// In minutes
+		
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrencyEuro = Currency.ValidCurrency.EURO;
+		Currency.ValidCurrency coinCurrencyDkk = Currency.ValidCurrency.DKK;
+		
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+		
+		coinValue = 50;
+		
+		coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
 	}
 
 
 
 	/** Fixture for pay station testing. */
-	@After
+	@AfterEach
 	public void cleanUp() {
 		ps.setReady();
 	}	
