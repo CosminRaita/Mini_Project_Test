be44a7957022366c537b71383b57a4e24b1afee3
AzizKadem <93520332+AzizKadem@users.noreply.github.com>
2022-02-23 12:39:38 +0100

    refactor pp ayment

--- PayStation/src/modellayer/PPayment.java
+++ PayStation/src/modellayer/PPayment.java
@@ -1,6 +1,7 @@
 package modellayer;
 
 import controllayer.IllegalCoinException;
+import utility.Validation;
 
 public class PPayment {
 
@@ -47,103 +48,11 @@ public class PPayment {
 		return timeBoughtInMinutes;
 	}
 	
-	public void validateCoin(int coinValue, Currency.ValidCurrency currency, Currency.ValidCoinType coinType) throws IllegalCoinException {
-
-		if (currency == Currency.ValidCurrency.EURO) {
-			boolean euroCoinOk = validateEuro(coinValue, coinType);
-			if (!euroCoinOk) {
-				throw new IllegalCoinException("Invalid Euro coin: " + coinValue);
-			}
-		} else if (currency == Currency.ValidCurrency.DKK) {
-			boolean dkkCoinOk = validateDkk(coinValue, coinType);
-			if (!dkkCoinOk) {
-				throw new IllegalCoinException("Invalid Dkk coin: " + coinValue);
-			}
-		} else {
-			throw new IllegalCoinException("Invalid coin: " + coinValue);
-		}
+	public void validateCoin(Coin coin) throws IllegalCoinException {
+		
+		Validation.validateCoin(coin);	
 	}
 
-	private boolean validateEuro(int coinValue, Currency.ValidCoinType coinType) {
-		boolean euroCoinOk = false;
-		
-		if (coinType == Currency.ValidCoinType.FRACTION) {
-			euroCoinOk = testCentCoin(coinValue);
-		}
-		if (coinType == Currency.ValidCoinType.INTEGER) {
-			euroCoinOk = testEuroCoin(coinValue);
-		}
-		
-		return euroCoinOk;
-	}
-	
-	private boolean validateDkk(int coinValue, Currency.ValidCoinType coinType) {
-		boolean dkkCoinOk = false;
-		
-		if (coinType == Currency.ValidCoinType.FRACTION) {
-			dkkCoinOk = testOreCoin(coinValue);
-		}
-		if (coinType == Currency.ValidCoinType.INTEGER) {
-			dkkCoinOk = testDkkCoin(coinValue);
-		}
-		
-		return dkkCoinOk;
-	}
-	
-	private boolean testCentCoin(int coinValue) {
-		boolean coinIsOk = true;
-		switch (coinValue) {
-			case 1:
-			case 2:
-			case 5:
-			case 10:
-			case 20:
-			case 50:
-				break;
-			default:
-				coinIsOk = false;
-		}
-		return coinIsOk;
-	}
-	
-	private boolean testEuroCoin(int coinValue) {
-		boolean coinIsOk = true;
-		switch (coinValue) {
-			case 1:
-			case 2:
-				break;
-			default:
-				coinIsOk = false;
-		}
-		return coinIsOk;
-	}	
-	
-	private boolean testOreCoin(int coinValue) {
-		boolean coinIsOk = true;
-		switch (coinValue) {
-			case 50:
-				break;
-			default:
-				coinIsOk = false;
-		}
-		return coinIsOk;
-	}
-	
-	private boolean testDkkCoin(int coinValue) {
-		boolean coinIsOk = true;
-		switch (coinValue) {
-			case 1:
-			case 2:
-			case 5:
-			case 10:
-			case 20:
-				break;
-			default:
-				coinIsOk = false;
-		}
-		return coinIsOk;
-	}	
-	
 	private double getEuroCoinValueInCent(double coinValue, Currency.ValidCoinType coinType) {
 		double valueInCent = 0;
 
