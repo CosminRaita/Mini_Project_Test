f5875ff9545ca432b74324edbf9a15ac1f368767
PieckaAndrej <93518895+PieckaAndrej@users.noreply.github.com>
2022-02-24 10:06:53 +0100

    moved calculation currency tests

--- PayStation/src/test/TestCalculationCurrencyEuro.java
+++ PayStation/src/test/TestCalculationCurrencyEuro.java
@@ -1,10 +1,14 @@
 package test;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import controllayer.*;
-import modellayer.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controllayer.ControlPayStation;
+import controllayer.IllegalCoinException;
+import modellayer.Currency;
 
 /**
  * Inspired by the book: Flexible, Reliable Software Henrik Bærbak Christensen:
@@ -16,44 +20,54 @@ public class TestCalculationCurrencyEuro {
 	private ControlPayStation ps;
 
 	/** Fixture for pay station testing. */
-	@Before
+	@BeforeEach
 	public void setUp() {
 		ps = new ControlPayStation();
 	}
 	
 	/**
-	 * Entering no coins should make the display report 0 minutes parking time.
+	 * Entering 1 euro should make the display report 40 minutes parking time.
 	 */
 	@Test
-	public void shouldDisplay0() throws IllegalCoinException {
+	public void shouldDisplay40MinFor1Euro() throws IllegalCoinException {
 		
 		// Arrange
-		int expectedParkingTime = 0;
+		int expectedParkingTime = 40;	// In minutes
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
 		
 		// Act
-		// No action
-		
+		ps.addPayment(coinValue, coinCurrency, coinType);
+			
 		// Assert
-		assertEquals("Should display 0 min for no coins", expectedParkingTime, ps.readDisplay());
-	}	
-
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
 	/**
-	 * Entering 5 cents should make the display report 2 minutes parking time
+	 * Entering 50 cents should make the display report 20 minutes parking time.
 	 */
 	@Test
-	public void shouldDisplay2MinFor5Cents() throws IllegalCoinException {
+	public void shouldDisplay20MinFor50cents() throws IllegalCoinException {
 		
-//		// Arrange
-//		int expectedParkingTime = 2;	// In minutes		
-//		int coinValue = 5;
-//		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
-//		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		// Arrange
+		int expectedParkingTime = 20;	// In minutes
+		int coinValue = 50;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
 		
 		// Act
-		
+		ps.addPayment(coinValue, coinCurrency, coinType);
+			
 		// Assert
-
+		assertEquals(expectedParkingTime, ps.readDisplay());
 	}
+	
+	/** Fixture for pay station testing. */
+	@AfterEach
+	public void cleanUp() {
+		ps.setReady();
+	}	
 
 	
 }
--- PayStation/src/test/TestCalculationCurrencyMixed.java
+++ PayStation/src/test/TestCalculationCurrencyMixed.java
@@ -1,10 +1,14 @@
 package test;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import controllayer.*;
-import modellayer.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controllayer.ControlPayStation;
+import controllayer.IllegalCoinException;
+import modellayer.Currency;
 
 /**
  * Inspired by the book: Flexible, Reliable Software Henrik Bærbak Christensen:
@@ -16,27 +20,67 @@ public class TestCalculationCurrencyMixed {
 	ControlPayStation ps;
 
 	/** Fixture for pay station testing. */
-	@Before
+	@BeforeEach
 	public void setUp() {
 		ps = new ControlPayStation();
 	}
 
 	/**
-	 * Entering 1 cent and 50 øre should make the display report 4 minutes parking time.
+	 * Entering 1 euro and 1 dkk should make the display report 46 minutes parking time.
 	 */
 	@Test
-	public void shouldDisplay4MinFor1CentAnd1Ore() throws IllegalCoinException {
+	public void shouldDisplay46MinFor1DkkAnd1Euro() throws IllegalCoinException {
+		
 		// Arrange
+		int expectedParkingTime = 46;	// In minutes
+		
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrencyEuro = Currency.ValidCurrency.EURO;
+		Currency.ValidCurrency coinCurrencyDkk = Currency.ValidCurrency.DKK;
+		
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
 		
 		// Act
-
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+			
 		// Assert
-		//assertEquals("Dummy", 0, 1);		
+		assertEquals(expectedParkingTime, ps.readDisplay());
+	}
+	
+	/**
+	 * Entering 1 euro, 1 dkk, 50 cents and 50 ore should make the display report 68 minutes parking time.
+	 */
+	@Test
+	public void shouldDisplay68MinFor1DkkAnd1EuroAnd50centsAnd50ore() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 68;	// In minutes
+		
+		int coinValue = 1;
+		Currency.ValidCurrency coinCurrencyEuro = Currency.ValidCurrency.EURO;
+		Currency.ValidCurrency coinCurrencyDkk = Currency.ValidCurrency.DKK;
+		
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+		
+		coinValue = 50;
+		
+		coinType = Currency.ValidCoinType.FRACTION;
+		
+		ps.addPayment(coinValue, coinCurrencyEuro, coinType);
+		ps.addPayment(coinValue, coinCurrencyDkk, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, ps.readDisplay());
 	}
 
 	
 	/** Fixture for pay station testing. */
-	@After
+	@AfterEach
 	public void cleanUp() {
 		ps.setReady();
 	}
